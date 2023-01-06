d9d4c23a0fdbc0254e5b77e7442f2c858a9be4ea
ablirysa <ysabellea@nycstudents.net>
2023-01-06 00:49:32 -0500

    finished

--- src/Treasure.java
+++ src/Treasure.java
@@ -1,5 +1,5 @@
 public class Treasure {
-    public static final String SCEPTOR = "the Mighty Sceptor";
+    public static final String SCEPTER = "the Mighty Scepter";
     public static final String CROWN = "the Crown";
     public static final String GEMS = "the Rubies";
     public static final String DUST = "Dust";
@@ -14,12 +14,12 @@ public class Treasure {
         } else if (num == 3) {
             treasure = CROWN;
         } else {
-            treasure = SCEPTOR;
+            treasure = SCEPTER;
         }
     }
 
     public static boolean collectionHasAllTreasures(String collection) {
-        boolean hasSceptor = (collection.indexOf(SCEPTOR) != -1);
+        boolean hasSceptor = (collection.indexOf(SCEPTER) != -1);
         boolean hasCrown = (collection.indexOf(CROWN) != -1);
         boolean hasGems = (collection.indexOf(GEMS) != -1);
         return hasSceptor && hasCrown && hasGems;
